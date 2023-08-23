2fa4fad86d5a6f874f6cc7aee5767d4a06d1631e
pierre <jobrider@gmail.com>
2023-08-23 13:46:07 +0200

    MAJ CSS Responsiver : OK

--- template-parts/filters.php
+++ template-parts/filters.php
@@ -59,11 +59,53 @@ var_dump($arrayUniqueCategory); */
 
 /**************************** SELECT TAGS (FORMAT) ********************************************/
 
+
+
+echo '<select id="formatSelect">';
+echo '<option value="" selected>FORMAT</option>';
+
+$data = $args['data'];// Le json complet de tous portfolio par defaut
+$arrayAllFormat = [];//initialiser le tableau qui prendra tous les formats (en fait tags)
+
+//Creation du tableau des categories uniques (toutes les cat uniques de portfolio par defaut)
+foreach ($data as $item) {
+
+    $tags = $item['tags'];
+
+    foreach ($tags as $tag) {
+
+        $tag = $tag['name'];
+
+        //Créer tableau simples
+        $arrayAllFormat[] = $tag;
+
+        //Créer tableau d'objets
+        /* $arrayAllCategory[] = [
+            'category' => $cat,
+        ]; */
+    
+    }
+}
+/* Comme on a pris tous les formats du tableau portfolio il faut les dedoublonner */
+$arrayUniqueFormat = array_values(array_unique($arrayAllFormat));//unique = dedoublonne - arrayvalue = réordonne correctement les index
+
+//Afficher le Seledt Format en html
+for ($i = 0; $i <= count($arrayUniqueFormat)-1; $i++){
+
+    echo '<option value="'. $arrayUniqueFormat[$i] .'">' . $arrayUniqueFormat[$i] .  '</option>';
+
+}
+
+echo '</select>';
+
+
+
+/* 
 echo '<select id="formatSelect">';
 echo '<option value="" selected>FORMATS</option>';
 echo '<option value="portrait">Portrait</option>';
 echo '<option value="paysage">Paysage</option>';
-echo '</select>';
+echo '</select>'; */
 
 /****************************** SELECT RECENT SORT ********************************************/
 
